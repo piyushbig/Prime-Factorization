9c23748f496894913798ef7cde808f109bcfed7d
piyushbig <74531950+piyushbig@users.noreply.github.com>
2022-01-19 11:19:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# prime factors
+
+import math
+
+n = int(input("enter a number: "))
+
+
+def primefact(n):
+    while n % 2 == 0:
+        print(2),
+        n = n / 2
+
+    for i in range(3, int(math.sqrt(n) + 1), 2):
+        while n % i == 0:
+            print(i),
+            n = n / i
+
+    if n > 2:
+        print(n)
+
+
+print(primefact(n))
